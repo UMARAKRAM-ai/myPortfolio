6b7c2d2816ba4d23bf8878a262b2697c61366b10
UMARAKRAM-ai <eng.umarakram@gmail.com>
2023-09-26 02:22:53 +0500

    hello

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+window.onscroll = () => {
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky' , window.scrollY > 100);
+}
+
+
+
+// menu icon
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar')
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+
+}
