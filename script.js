0044bb376b4eec9c77f74a6a37bd840c608130f3
UMARAKRAM-ai <eng.umarakram@gmail.com>
2023-09-26 19:04:20 +0500

    Image Slideshow added

--- script.js
+++ script.js
@@ -18,4 +18,34 @@ menuIcon.onclick = () =>{
 }
 
 
+function change(){
+    var Img = document.getElementById("home");
+    Img.style.backgroundImage="url(images/education.jpg)"
+}
 
+function change2(){
+    var Img = document.getElementById("home");
+    Img.style.backgroundImage="url(images/bg.jpg)"
+}
+
+
+// For about Image Slide Showw
+var images = [
+    "images/nubelson-fernandes-jKL2PvKN8Q0-unsplash.jpg",
+    "images/port3.jpg", 
+    "images/port3.jpg",
+    "images/port4.jpg",
+    "images/port5.jpg",
+    "images/port6.jpg"
+  ];
+  
+  var currentImageIndex = 0;
+  function changeImage() {
+    var slideshowImage = document.getElementById("slideshow");
+    slideshowImage.src = images[currentImageIndex];
+    
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+  }
+  
+  setInterval(changeImage, 1900); 
+  
